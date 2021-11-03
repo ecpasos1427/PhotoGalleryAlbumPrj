ed5a7348b386cdef5858da9a6239586feb240697
ecpasos1427 <epasos1427@gmail.com>
2021-11-03 13:20:13 +0800

    Update the welcome construction page

---
+++ src/containers/WelcomeConstructionView.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class WelcomeConstructionView extends React.Component {
+  state = {};
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div>
+        <h1>Welcome!</h1>
+        <br />
+        <h5>
+          We are still currently working on this web site and we are happy to
+          see you soon!!!
+        </h5>
+      </div>
+    );
+  }
+}
+
+export default WelcomeConstructionView;
--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,13 @@ import { Route } from "react-router-dom";
 
 import ArticleListView from "./containers/ArticleListView";
 import ArticleDetailView from "./containers/ArticleDetailView";
+import WelcomeConstructionView from "./containers/WelcomeConstructionView";
 
 const BaseRouter = () => (
   <div>
-    <Route exact path="/" component={ArticleListView} />
-    <Route exact path="/:articleID" component={ArticleDetailView} />
+    <Route exact path="/" component={WelcomeConstructionView} />
+    <Route exact path="/articles/" component={ArticleListView} />
+    <Route exact path="/article/:articleID" component={ArticleDetailView} />
   </div>
 );
 
